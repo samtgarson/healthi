e7aa817fed26e303690c532eee630c8261a277fc
Sam <samtgarson@gmail.com>
2014-02-08 16:38:39 +0000

    home structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
 })
+// function nearest(a, n) {
+// 	var curr = a[0]
+// 	for (i = 0; i < a.length; ++i) {
+// 		v = a[i]
+// 		if (Math.abs(n - v) < Math.abs(n - curr)) {
+// 			curr = v
+// 		}
+// 		return curr
+// 	}
+// }
+function nearest (num, arr) {
+	var res = arr.reduce(function (prev, curr) {
+	  return (Math.abs(curr - num) < Math.abs(prev - num) ? curr : prev);
+	});
+}
