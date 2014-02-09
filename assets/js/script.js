c4620824b530bb0d095117069dd7fa21ebeb2701
Sam <samtgarson@gmail.com>
2014-02-09 15:51:21 +0000

    more nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-	
-
-})
+    $('.slider-nav ul').pep({
+        axis: 'x',
+        initiate: function() {console.log('started')}
+    });
+});
 // function nearest(a, n) {
 // 	var curr = a[0]
 // 	for (i = 0; i < a.length; ++i) {
@@ -14,6 +16,6 @@ $(document).ready(function() {
 // }
 function nearest (num, arr) {
 	var res = arr.reduce(function (prev, curr) {
-	  return (Math.abs(curr - num) < Math.abs(prev - num) ? curr : prev);
+    return (Math.abs(curr - num) < Math.abs(prev - num) ? curr : prev);
 	});
 }
